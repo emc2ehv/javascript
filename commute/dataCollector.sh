91cf6d1b2521e34c8be412411e895733819ea889
Mayank Patel <mayank_sesame@yahoo.com>
2020-03-21 13:41:20 -0400

    Update dataCollector.sh

--- commute/dataCollector.sh
+++ commute/dataCollector.sh
@@ -2,6 +2,8 @@
 
 outputLocation=$1
 key=$2
-toLM=`curl -s "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=216+Armata+Drive+Middletown+DE&destinations=750+Centerton+Road+Mount+Laurel+NJ&departure_time=now&key=${key}" | jq .rows[0].elements[0].duration_in_traffic.value`
-fromLM=`curl -s "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=750+Centerton+Road+Mount+Laurel+NJ&destinations=216+Armata+Drive+Middletown+DE&departure_time=now&key=${key}" | jq .rows[0].elements[0].duration_in_traffic.value`
+from=$3
+to=$4
+toLM=`curl -s "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${from}&destinations=${to}&departure_time=now&key=${key}" | jq .rows[0].elements[0].duration_in_traffic.value`
+fromLM=`curl -s "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${to}&destinations=${from}&departure_time=now&key=${key}" | jq .rows[0].elements[0].duration_in_traffic.value`
 echo -e "`date`,${toLM},${fromLM}" >> ${outputLocation}
