f34c5f84446229b71afbbef129e4944f2e1bcd54
Mayank Patel <mayank_sesame@yahoo.com>
2021-12-18 12:54:49 -0500

    Update dataCollector.sh

--- commute/dataCollector.sh
+++ commute/dataCollector.sh
@@ -6,4 +6,4 @@ from=${3}
 to=${4}
 toWork=`curl -s "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${from}&destinations=${to}&departure_time=now&key=${key}" | jq .rows[0].elements[0].duration_in_traffic.value`
 fromWork=`curl -s "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${to}&destinations=${from}&departure_time=now&key=${key}" | jq .rows[0].elements[0].duration_in_traffic.value`
-echo -e "`date`,$((toWork/60)),$((fromWork/60))" >> ${outputLocation}
+echo -e "`date +"%a %d %b %Y %I:%M:%S %p %Z"`,$((toWork/60)),$((fromWork/60))" >> ${outputLocation}
